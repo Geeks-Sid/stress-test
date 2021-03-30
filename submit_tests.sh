8cddc9c01a8c460e3ada5740fdd8a2c998f597f0
Siddhesh Thakur <sid.cre8er@gmail.com>
2021-03-31 04:17:36 +0530

    bash file for parallel submission

---
+++ submit_tests.sh
@@ -0,0 +1,17 @@
+patch_sizes="64 128 256 512 1024 2048 4096";
+batch_sizes="24576 6144 1536 384 96 24 6";
+
+patch_sizes=($patch_sizes)
+batch_sizes=($batch_sizes)
+
+base_csv=$1
+ref_csv=$2
+threads=28
+
+count=${#batch_sizes[@]}
+
+for i in `seq 1 $count`
+do
+    echo ${patch_sizes[$i-1]} ${batch_sizes[$i-1]}
+    echo openslide-test-v1.py -i $base_csv -r $ref_csv -b ${batch_sizes[$i-1]} -p ${patch_sizes[$i-1]} -t $threads
+done
