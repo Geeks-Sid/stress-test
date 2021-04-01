d2b8e9b0be037f869cc5fd02b5952b8c6d10c380
sarthakpati <sarthak.pati@hotmail.com>
2021-04-01 07:51:15 -0400

    syntax fix

--- submitter_tests_gandlf.py
+++ submitter_tests_gandlf.py
@@ -5,6 +5,6 @@ thread_sizes=[1,2,4,8,16,32,64]
 
 for b in batch_sizes:
   for t in thread_sizes:
-    command = 'python gandlf-test-v1.py -i ~/projects_wsl/stress-test/data.csv -b ' + str(b) + ' -t ' + str(t)'
+    command = 'python gandlf-test-v1.py -i ~/projects_wsl/stress-test/data.csv -b ' + str(b) + ' -t ' + str(t)
     print(command)
     # subprocess.Popen(command)
